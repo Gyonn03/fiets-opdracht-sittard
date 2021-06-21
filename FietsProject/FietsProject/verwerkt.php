28fd4271e9f0ea7789e56d3b7a0c10ed75068853
TheTrumpetist27 <56255204+TheTrumpetist27@users.noreply.github.com>
2021-06-21 15:29:01 +0200

    Delete FietsProject/FietsProject directory

--- FietsProject/FietsProject/verwerkt.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include("functions.php");
-
-    $result = GetData("INSERT INTO vragenlijst (vraag_een, vraag_twee) VALUES ('". $_POST["woonplaats"] ."', '". $_POST["stad"] ."')");
-
-    header("location: index.php");
-    exit;
