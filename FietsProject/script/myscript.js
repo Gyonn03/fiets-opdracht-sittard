7e90d8b3d5632accf97d82e5eaea3edfdb566727
TheTrumpetist27 <56255204+TheTrumpetist27@users.noreply.github.com>
2021-06-25 13:49:11 +0200

    Code beetje opgeschoond

--- FietsProject/script/myscript.js
+++ FietsProject/script/myscript.js
@@ -1,17 +1,17 @@
 $(function() {
     $( "#button" ).click(function() {
-      $( "#button" ).addClass( "onclic", 250, validate);
+        $( "#button" ).addClass( "onclic", 250, validate);
     });
-  
+
     function validate() {
       setTimeout(function() {
         $( "#button" ).removeClass( "onclic" );
         $( "#button" ).addClass( "validate", 450, callback );
       }, 2250 );
     }
-      function callback() {
+    function callback() {
         setTimeout(function() {
-          $( "#button" ).removeClass( "validate" );
+            $( "#button" ).removeClass( "validate" );
         }, 1250 );
-      }
-    });
+    }
+}); 
