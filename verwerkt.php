4e5e2b3a9fb52562fc96197f1621e7c005799f0a
TheTrumpetist27 <56255204+TheTrumpetist27@users.noreply.github.com>
2021-06-29 11:41:20 +0200

    minor update aan de query

---
+++ verwerkt.php
@@ -0,0 +1,8 @@
+<?php
+    include("functions.php");
+
+    $result = GetData("INSERT INTO vragenlijst (antwoord_een, antwoord_twee) VALUES ('". $_POST["woonplaats"] ."', '". $_POST["stad"] ."')");
+
+    header("location: index.php");
+    exit;
+?>
